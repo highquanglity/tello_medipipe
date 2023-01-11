8c4011a47590fac0b8561aaae5d43a25db0f9eb2
Nghiêm Văn Quang <91304338+highquanglity@users.noreply.github.com>
2023-01-11 13:48:23 +0700

    Push all

--- gestures/tello_keyboard_controller.py
+++ gestures/tello_keyboard_controller.py
@@ -3,8 +3,10 @@ from djitellopy import Tello
 class TelloKeyboardController:
     def __init__(self, tello: Tello):
         self.tello = tello
+        self.in_flight=False
 
     def control(self, key):
+
         if key == ord('w'):
             self.tello.move_forward(30)
         elif key == ord('s'):
@@ -21,6 +23,16 @@ class TelloKeyboardController:
             self.tello.move_up(30)
         elif key == ord('f'):
             self.tello.move_down(30)
-
+        elif key == 0:
+            self.tello.flip_forward()
+        elif key ==1:
+            self.tello.flip_back()
+        elif key == 2:
+            self.tello.flip_left()
+        elif key == 3:
+            self.tello.flip_right()
+        elif key == ord('u'):
+            self.tello.emergency()
+       
 
 
